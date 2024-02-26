7aad07f7dcd661423f93bb2e62e2dc5f1dfe88f4
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2024-02-26 16:31:28 +0530

    React App

---
+++ Phase-1/Session-13/firstapp/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+
+function App() {
+  return (
+    <div className='container'>
+      <h1 className='text-primary text-center'>First React Bootstrap App</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ Phase-1/Session-13/firstapp/src/components/Footer.js
@@ -0,0 +1,24 @@
+
+import React from "react";
+
+
+function Footer(){
+
+
+    return(
+        <div className="container-fluid bg-dark">
+            <div className="row">
+                <div className="col text-center text-primary mt-5 mr-5 mb-5 ml-5">
+
+                        <h4 className="text-center text-warning">Footer Component</h4>
+                        <h5>@copyright 2024 | Prepared by: Nikunj Soni</h5>
+
+                </div>
+            </div>
+
+        </div>
+    )
+
+}
+
+export default Footer;
