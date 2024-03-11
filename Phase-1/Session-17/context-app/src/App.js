af1adaf755b584443851a21e58ec103643d525f8
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2024-03-11 16:18:33 +0530

    useContext

--- Phase-1/Session-17/context-app/src/App.js
+++ Phase-1/Session-17/context-app/src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import GrandParent from './components/usecase-2/GrandParent';
+import Userinfo from './components/usecase-3/Userinfo';
+import { AuthProvide } from './components/usecase-3/AuthContext';
 
 function App() {
   return (
     <div>
       <h1>useContext API</h1>
-      <GrandParent/>
+    
+      <AuthProvide>
+        <h3>Authentication App</h3>
+        <Userinfo/>
+      </AuthProvide>
 
     </div>
   );
---
+++ Phase-1/Session-17/context-app/src/components/usecase-3/AuthContext.js
@@ -0,0 +1,30 @@
+const { createContext, useState, useContext } = require("react");
+
+const AuthContext= createContext();
+
+export const AuthProvide=({children})=>{
+    const [user,setUser]=useState(null);
+    const login=async (userData)=>{
+        console.log(userData);
+
+        setUser(userData)
+        
+        //API call of Backend for cheking the user (API is available in JAVA Backend)
+        // resp=await axios.post("http://localhost:5000/api/auth/login",userData)
+        // if(resp.status===200){
+        //     setUser(resp.data.userDetails);
+        // }
+    }
+    const logout=()=>{
+        //api call for logout
+        setUser(null);
+    }
+    return(
+        <AuthContext.Provider value={{user,login,logout}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+export const useAuthContext=()=>{
+    return useContext(AuthContext);
+}
