f847c70fa0593fb9f353c726c8cb9b6a8077742a
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2024-03-12 14:59:15 +0530

    useReducer

---
+++ Phase-1/Session-18/reducer-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Counter from './component/Counter';
+
+function App() {
+  return (
+    <div>
+      <Counter/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Phase-1/Session-18/reducer-app/src/component/Counter.js
@@ -0,0 +1,34 @@
+
+import { useReducer } from 'react';
+
+const reducer=(state,action)=>{
+    switch (action.type) {
+        case 'INCREMENT':
+            return {count:state.count+1}
+            break;
+        case 'DECREMENT':
+            return {count:state.count-1}
+            break;
+    
+        default:
+            return state;
+    }
+}
+
+
+function Counter(){
+    const [state,dispatch]=useReducer(reducer,{count:0});
+    return(
+        <div>
+            <h2>Counter App Using Reducer</h2>
+
+            <p>Count: {state.count}</p>
+
+            <button onClick={()=>dispatch({type:'INCREMENT'})}>+</button>
+            <button onClick={()=>dispatch({type:'DECREMENT'})}>-</button>
+
+        </div>
+    )
+}
+
+export default Counter;
